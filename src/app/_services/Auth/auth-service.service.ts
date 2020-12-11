ecb1876a3a35aca4104a0d4f37a21bd160261f72
everescent <35443878+everescent@users.noreply.github.com>
2020-12-11 11:38:38 +0800

    Added Auth service and api key interceptor.

---
+++ src/app/_services/Auth/auth-service.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+  readonly AUTH_API_LOGIN: string = 'https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/login';
+
+  constructor(private http: HttpClient) { }
+
+  login(user: string, pass: string): Observable<any> {
+    const cred = {
+      username: user,
+      password: pass
+    };
+
+    return this.http.post(`${this.AUTH_API_LOGIN}login`, cred).pipe(map(user => {
+
+    }));
+  }
+}
