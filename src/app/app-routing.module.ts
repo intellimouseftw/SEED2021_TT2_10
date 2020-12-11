2e2b88273b0bc0df1291eb7928c76c03f9596951
Syukrirahiman <43146706+Syukrirahiman@users.noreply.github.com>
2020-12-11 12:18:17 +0800

    Added Transfer Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TransferComponent } from './transfer/transfer.component';
+
 
 
 const routes: Routes = [
   // {path: 'profile', component: null},
   // {path: 'transactionhistory', component: null},
-  // {path: 'transfer', component: null },
+  {path: 'transfer', component: TransferComponent },
   // {path: 'balance', component: null },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import  { MatButtonModule } from '@angular/material/button';
 
 import { NavbarComponent } from './navbar/navbar.component';
+import { TransferComponent } from './transfer/transfer.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    routingComponents
+    routingComponents,
+    TransferComponent
   ],
   imports: [
     BrowserModule,
