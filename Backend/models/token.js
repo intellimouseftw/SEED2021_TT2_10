2d488d7f18a4157e4ec6f075f8c0da59b149477f
Gerald Tay <yangjietay@gmail.com>
2020-12-11 15:53:13 +0800

    added routes to DBS API /transaction/add and /transaction/view

--- Backend/models/token.js
+++ Backend/models/token.js
@@ -4,12 +4,6 @@ const Schema = mongoose.Schema;
 const tokenSchema = new Schema({
   userId: { type: String, required: true },
   refreshToken: { type: String, required: true },
-  // nric: { type: String, required: true },
-  // gender: { type: String, required: true },
-  // age: { type: String, required: true },
-  // phoneNumber: { type: String, required: true },
-  // email: { type: String, required: true },
-  // address: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Token", tokenSchema);
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -4,13 +4,6 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
-  // token: { type: String },
-  // nric: { type: String, required: true },
-  // gender: { type: String, required: true },
-  // age: { type: String, required: true },
-  // phoneNumber: { type: String, required: true },
-  // email: { type: String, required: true },
-  // address: { type: String, required: true },
 });
 
 module.exports = mongoose.model("User", userSchema);
