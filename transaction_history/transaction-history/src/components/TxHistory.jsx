94920a236f84cd1bb0338d451b5b368fd80002ab
intellimouseftw <intellimouseftw@hotmail.com>
2020-12-11 15:04:35 +0800

    Added transaction filtering function

--- transaction_history/transaction-history/src/components/TxHistory.jsx
+++ transaction_history/transaction-history/src/components/TxHistory.jsx
@@ -2,17 +2,15 @@ import React, { useState, useEffect } from 'react'
 import { getTxData } from '../api/getTxData.api'
 import { getUserData } from '../api/getUserData.api'
 import TxDetails from './TxDetails'
+import '../css/TxHistory.css'
 
 import { txData, userDBdata } from '../static_data'
 
 function TxHistory() {
 
-    const custID = 10
-    const [txHist, setTxHist] = useState([])
+    const [txHist, setTxHist] = useState(txData)
 
-    const [userDB, setUserDB] = useState([])
-
-    // console.log(getTxData(10))
+    const [userDB, setUserDB] = useState(userDBdata)
 
     // useEffect(() => {
     //     getTxData(custID).then((response) => {
@@ -28,28 +26,79 @@ function TxHistory() {
 
     const [isExpanded, setExpanded] = useState(false)
 
+    function filterDB(txHist, startDateStr, endDateStr) {
+        const [syear, smonth, sday] = startDateStr.split('-')
+        const [eyear, emonth, eday] = endDateStr.split('-')
+        const startDate = new Date(syear, smonth, sday)
+        const endDate = new Date(eyear, emonth, eday)
+
+
+        return (txData.filter((txData) => {
+            const txDateStr = txData.dateTime
+            const [txyear, txmonth, txday] = txDateStr.substring(0, 10).split('-')
+            const txDate = new Date(txyear, txmonth, txday)
+
+            return txDate >= startDate && txDate <= endDate
+        }))
+    }
+
+    function handleDateFilter(event) {
+        event.preventDefault()
+        setTxHist(filterDB(txHist, event.target[0].value, event.target[1].value))
+    }
+
+    function handleReset() {
+        setTxHist(txData)
+    }
 
     return (
         <div className="container-fluid">
-        <h1>Transaction History</h1>
-        {isExpanded ? 
-        <div className="container-fluid text-center">
-            {txData.map((singleTx, txID) => {
-                return (
-                    <TxDetails data={singleTx} txID={txID+1} userDB={userDBdata} />
-                )
-            })}
-        </div> :
-        <div className="container-fluid text-center">
-            {txData.slice(0,3).map((singleTx, txID) => {
-                return (
-                    <TxDetails data={singleTx} txID={txID+1} userDB={userDBdata} />
-                )
-            })}
-            <button className="btn btn-outline-primary" onClick={()=>setExpanded(true)}>View All Transactions...</button>
-        </div>
-        }
-        </div>
+            <h1>Transaction History</h1>
+            <div className="container-fluid">
+                <form className="form-group" onSubmit={handleDateFilter}>
+                    <div className="row">
+                        <div className="col">
+                        <b>Filter transactions by date</b>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            Start Date
+                            <input className="form-control" type="date" id="startDate" required></input>
+                        </div>
+                        <div className="col">
+                            End Date
+                            <input className="form-control" type="date" id="endDate" required></input>
+                        </div>
+                    </div>
+                    <div className="row text-center">
+                        <div className="col">
+                            <button className="form-control btn btn-primary btn-sm btn_width" type="submit">Filter</button>
+                            <button className="form-control btn btn-outline-primary btn-sm btn_width" type="reset" onClick={handleReset}>Reset</button>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+            {
+                isExpanded ?
+                    <div className="container-fluid text-center">
+                        {txHist.map((singleTx, txID) => {
+                            return (
+                                <TxDetails data={singleTx} txID={txID + 1} userDB={userDB} />
+                            )
+                        })}
+                    </div> :
+                    <div className="container-fluid text-center">
+                        {txHist.slice(0, 3).map((singleTx, txID) => {
+                            return (
+                                <TxDetails data={singleTx} txID={txID + 1} userDB={userDB} />
+                            )
+                        })}
+                        {txHist.length > 3 && <button className="btn btn-outline-primary" onClick={() => setExpanded(true)}>View All Transactions...</button>}
+                    </div>
+            }
+        </div >
     )
 }
 
